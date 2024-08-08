6e5a733da69b8af54c8a4626e01e485f29830780
japa <guilherme.nakano@escola.pr.gov.br>
2024-08-08 20:10:48 -0300

    update8

--- script/script.js
+++ script/script.js
@@ -2,7 +2,7 @@
 (function update() {
     $.getJSON( "https://api.weather.com/v2/pws/observations/current?stationId=IJANDA6&format=json&units=m&apiKey=2fa2cac154b640b1a2cac154b680b15f&numericPrecision=decimal")
     .done(function( json ) {
-        //Chamando as informações da estação meteorológica
+        //Atribuindo as informações da estação meteorológica
         $('#windgust').text(json.observations[0].metric.windGust);
         $('#windspeed').text(json.observations[0].metric.windSpeed);
         $('#temp').text(json.observations[0].metric.temp);
@@ -13,17 +13,22 @@
         $('#preciprate').text(json.observations[0].metric.precipRate);
         $('#preciptotal').text(json.observations[0].metric.precipTotal);
     })
-    //Fazendo atualizar e mensagem de erro
+    //Exibir mensagem no console em caso de erro
     .fail(function( jqxhr, textStatus, error ) {
         var err = textStatus + ", " + error;
         console.log( "Request Failed: " + err );
+    //Buscar dados a cada 30s
     }).then(function() {
         setTimeout(update, 30000);
     });
 })(); 
 
-//Chamando a I.A 
+//Gerar texto por a I.A 
 function textIA(str) {
+
+    $('#textIA>p').text('');
+    $('#textIA').show();
+    $('#textIA>div.spinner-border').show();
     var settings = {
         "url": "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=AIzaSyDdXcVm_3NQ4nhlQ41Vl8-m-9SGhiYhfPY",
         "method": "POST",
@@ -46,9 +51,19 @@ function textIA(str) {
       
       $.ajax(settings)
         .done(function (response) {
-            $('#textIA').text(response.candidates[0].content.parts[0].text);
+            $('#textIA>p').text(response.candidates[0].content.parts[0].text);
+            $('#textIA>div.spinner-border').hide();
         });
 }
 
-//pergunta I.A
-textIA('A temperatura em Jandaia do Sul está 23°C. Descreva em apenas 30 palavras, qual a recomendação agronômica para o cultivo de soja nesta condição climática');
+$('#cultivo').on('change', function(){
+  if($('#cultivo').val() != 0) {
+    textIA('A temperatura em Jandaia do Sul está ' + $('#temp').text() + '°C, a velocidade do vento ' + $('#windspeed').text() + ' km/h e a umidade ' + $('#humidity').text() + '. Descreva em apenas 100 palavras, qual a recomendação agronômica para o cultivo de ' + $('#cultivo').val() + ' nesta condição climática');
+  } else {
+    $('#textIA').hide();
+    const toastIA = document.getElementById('toastIA')
+    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastIA)
+    toastBootstrap.show()
+  }
+});
+
